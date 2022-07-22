e360ab658cbd948c704b2f687ce24d676ca4e2f8
Neil Martin <neilmartindotdev@protonmail.com>
2022-07-22 22:45:59 +0100

    added funky background

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const password1 = document.getElementById("password1");
 const password2 = document.getElementById("password2");
  
 function validateForm(){
-    //First Name Validation 
-    var em=document.getElementById('email').value;
-    if(em == ""){
+    //Basic Validation 
+    var email=document.getElementById('email').value;
+    if(email == ""){
         alert('Email');
         document.getElementById('email').style.borderColor = "red";
         return false;
@@ -15,12 +15,39 @@ function validateForm(){
         document.getElementById('email').style.borderColor = "green";
     }
 
-    var cunt=document.getElementById('country').value;
-    if(cunt == ""){
+    var country=document.getElementById('country').value;
+    if(country == ""){
         alert('Country');
         document.getElementById('country').style.borderColor = "red";
         return false;
     }else{
         document.getElementById('country').style.borderColor = "green";
+    }
+
+    var zip=document.getElementById('zip').value;
+    if(zip == ""){
+        alert('Zip');
+        document.getElementById('zip').style.borderColor = "red";
+        return false;
+    }else{
+        document.getElementById('zip').style.borderColor = "green";
+    }
+
+    var password1=document.getElementById('password1').value;
+    if(password1 == ""){
+        alert('Invalid password 1 field');
+        document.getElementById('password1').style.borderColor = "red";
+        return false;
+    }else{
+        document.getElementById('password1').style.borderColor = "green";
+    }
+
+    var password2=document.getElementById('password2').value;
+    if(password2 == ""){
+        alert('Invalid password 2 field');
+        document.getElementById('password2').style.borderColor = "red";
+        return false;
+    }else{
+        document.getElementById('password2').style.borderColor = "green";
     }
 }
